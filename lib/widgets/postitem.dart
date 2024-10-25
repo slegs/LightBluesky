60509bf5520b37ae5e8cfe28e93ea49a3a42b4ce
Pablo Ferreiro <me@pabloferreiro.es>
2024-10-25 12:57:33 +0200

    (basic) repost and like implemented

--- lib/widgets/postitem.dart
+++ lib/widgets/postitem.dart
@@ -1,5 +1,7 @@
 import 'package:bluesky/bluesky.dart' as bsky;
+import 'package:bluesky/core.dart';
 import 'package:flutter/material.dart';
+import 'package:lightbluesky/common.dart';
 import 'package:lightbluesky/helpers/ui.dart';
 import 'package:lightbluesky/models/embedwrapper.dart';
 import 'package:lightbluesky/pages/post.dart';
@@ -8,18 +10,48 @@ import 'package:lightbluesky/widgets/embed.dart';
 import 'package:lightbluesky/widgets/icontext.dart';
 
 /// Card containing a FeedView (post)
-class PostItem extends StatelessWidget {
+class PostItem extends StatefulWidget {
   const PostItem({super.key, required this.item, this.reason});
 
   final bsky.Post item;
   final bsky.Reason? reason;
 
+  @override
+  State<PostItem> createState() => _PostItemState();
+}
+
+class _PostItemState extends State<PostItem> {
+  int _likes = 0;
+  int _reposts = 0;
+
+  AtUri? _userRepostedAtUri;
+  bool _userReposted = false;
+  AtUri? _userLikedAtUri;
+  bool _userLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.item.isReposted) {
+      _userRepostedAtUri = widget.item.viewer.repost!;
+      _userReposted = widget.item.isReposted;
+    }
+
+    if (widget.item.isLiked) {
+      _userLikedAtUri = widget.item.viewer.like!;
+      _userLiked = widget.item.isLiked;
+    }
+
+    _likes = widget.item.likeCount;
+    _reposts = widget.item.repostCount;
+  }
+
   Widget _handleReason() {
     String text;
     IconData icon;
 
-    if (reason!.data is bsky.ReasonRepost) {
-      final repost = reason!.data as bsky.ReasonRepost;
+    if (widget.reason!.data is bsky.ReasonRepost) {
+      final repost = widget.reason!.data as bsky.ReasonRepost;
 
       text = 'Reposted by ${repost.by.displayName ?? "@${repost.by.handle}"}';
       icon = Icons.autorenew;
@@ -34,6 +66,48 @@ class PostItem extends StatelessWidget {
     );
   }
 
+  Future<void> _handleRepost() async {
+    if (_userReposted) {
+      // Remove repost
+      await api.atproto.repo.deleteRecord(uri: _userRepostedAtUri!);
+      _userRepostedAtUri = null;
+    } else {
+      // Make repost
+      final res = await api.feed.repost(
+        cid: widget.item.cid,
+        uri: widget.item.uri,
+      );
+
+      _userRepostedAtUri = res.data.uri;
+    }
+
+    setState(() {
+      _userReposted = !_userReposted;
+      _reposts = _userReposted ? _reposts + 1 : _reposts - 1;
+    });
+  }
+
+  Future<void> _handleLike() async {
+    if (_userLiked) {
+      // Remove like
+      await api.atproto.repo.deleteRecord(uri: _userLikedAtUri!);
+      _userLikedAtUri = null;
+    } else {
+      // Make like
+      final res = await api.feed.like(
+        cid: widget.item.cid,
+        uri: widget.item.uri,
+      );
+
+      _userLikedAtUri = res.data.uri;
+    }
+
+    setState(() {
+      _userLiked = !_userLiked;
+      _likes = _userLiked ? _likes + 1 : _likes - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -41,29 +115,29 @@ class PostItem extends StatelessWidget {
         onTap: () {
           Ui.nav(
             context,
-            PostPage(uri: item.uri),
+            PostPage(uri: widget.item.uri),
           );
         },
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (reason != null) _handleReason(),
+            if (widget.reason != null) _handleReason(),
             // START Author's data
             ListTile(
               onTap: () {
-                Ui.nav(context, ProfilePage(did: item.author.did));
+                Ui.nav(context, ProfilePage(did: widget.item.author.did));
               },
               leading: CircleAvatar(
-                backgroundImage: item.author.avatar != null
-                    ? NetworkImage(item.author.avatar!)
+                backgroundImage: widget.item.author.avatar != null
+                    ? NetworkImage(widget.item.author.avatar!)
                     : null,
               ),
-              title: Text(item.author.displayName != null
-                  ? item.author.displayName!
-                  : '@${item.author.handle}'),
-              subtitle: item.author.displayName != null
-                  ? Text('@${item.author.handle}')
+              title: Text(widget.item.author.displayName != null
+                  ? widget.item.author.displayName!
+                  : '@${widget.item.author.handle}'),
+              subtitle: widget.item.author.displayName != null
+                  ? Text('@${widget.item.author.handle}')
                   : null,
             ),
             Padding(
@@ -71,15 +145,15 @@ class PostItem extends StatelessWidget {
                 left: 10.0,
               ),
               child: Text(
-                item.record.text,
+                widget.item.record.text,
               ),
             ),
             // END Author's data
             // Add embed if available
-            if (item.embed != null)
+            if (widget.item.embed != null)
               Embed(
                 wrap: EmbedWrapper.fromApi(
-                  root: item.embed!,
+                  root: widget.item.embed!,
                 ),
               ),
             // START interaction buttons
@@ -88,31 +162,31 @@ class PostItem extends StatelessWidget {
               children: [
                 TextButton.icon(
                   icon: const Icon(Icons.reply_outlined),
-                  label: Text(item.replyCount.toString()),
+                  label: Text(widget.item.replyCount.toString()),
                   onPressed: () {
                     Ui.nav(
                       context,
-                      PostPage(uri: item.uri, autoReply: true),
+                      PostPage(uri: widget.item.uri, autoReply: true),
                     );
                   },
                 ),
                 TextButton.icon(
-                  icon: item.isReposted
+                  icon: _userReposted
                       ? const Icon(Icons.autorenew)
                       : const Icon(Icons.autorenew_outlined),
-                  label: Text(item.repostCount.toString()),
-                  onPressed: () {
-                    Ui.snackbar(context, "TODO: Add repost");
-                  },
+                  label: Text(
+                    _reposts.toString(),
+                  ),
+                  onPressed: _handleRepost,
                 ),
                 TextButton.icon(
-                  icon: item.isLiked
+                  icon: _userLiked
                       ? const Icon(Icons.favorite)
                       : const Icon(Icons.favorite_outline),
-                  label: Text(item.likeCount.toString()),
-                  onPressed: () {
-                    Ui.snackbar(context, "TODO: Add like");
-                  },
+                  label: Text(
+                    _likes.toString(),
+                  ),
+                  onPressed: _handleLike,
                 ),
               ],
             ),
